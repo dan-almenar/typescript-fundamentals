26d8aff008a5f80908170e74d65c94c5a2a6df93
dan <danielalmenar@gmail.com>
2021-12-15 07:35:23 -0300

    more info added, covering functions, objects and interfaces

--- dist/index.js
+++ dist/index.js
@@ -44,3 +44,47 @@ var RandomEnum;
     RandomEnum[RandomEnum["age"] = 42] = "age";
 })(RandomEnum || (RandomEnum = {}));
 console.log('RandomEnum:', RandomEnum);
+const person = {
+    firstName: 'Dan',
+    lastName: 'Almenar',
+    age: 42
+};
+let firstPoint = 1;
+let secondPoint = 'hello';
+console.log('Point object examples (number and string):', firstPoint, secondPoint);
+const danDeveloper = {
+    firstName: 'Dan',
+    lastName: 'Almenar',
+    age: 42
+};
+console.log('person:', person);
+console.log('danDeveloper  object:', danDeveloper);
+let randomVariable = '3.1416';
+let piNumber1 = randomVariable;
+let piNumber2 = randomVariable;
+console.log('piNumber1:', piNumber1);
+console.log('piNumber2:', piNumber2);
+console.log('types:', typeof (piNumber1), typeof (piNumber2));
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+function addToString(num1, num2) {
+    return `The result is: ${(num1 + num2)}`;
+}
+function log(message) {
+    console.log(message);
+}
+console.log(addNumbers(4, 3));
+console.log(addToString(4, 3));
+log("hello world");
+const newUser = {
+    firstName: 'Dan',
+    lastName: 'Almenar',
+    age: 42
+};
+console.log('newUser (PersonInterface) .firstName:', newUser.firstName);
+console.log('newUser.hobbies (not declared):', newUser.hobbies);
+newUser.lastName = 'Developer';
+newUser.hobbies = ['programming', 'playing with his kids'];
+const add = (num1, num2) => num1 + num2;
+const substract = (num1, num2) => num1 - num2;
--- src/index.ts
+++ src/index.ts
@@ -56,6 +56,7 @@ console.log('updated randomTupple:', randomTupple)
 let tuppleArray: [string, number, boolean][] = [['hello', 1, true], ['world', 2, false]]
 console.log('tuppleArray:', tuppleArray)
 
+// UNIONS
 /*
 Unions allow a variable to hold multiple types.
 */
@@ -64,6 +65,7 @@ console.log('randomUnion first value:', randomUnion)
 randomUnion = 46
 console.log('updated randomUnion:', randomUnion)
 
+// ENUMS
 /*
 Enums allow you to define a set of named constants (thus, cannot be reassigned).
 It compiles into a JavaScript object.
@@ -85,3 +87,124 @@ enum RandomEnum {
     age = 42,
 }
 console.log('RandomEnum:', RandomEnum)
+
+// OBJECTS
+/*
+Objects can be declared with typed properties or as a type itself
+and then created a variable of said type:
+*/
+
+// first approach:
+const person: {
+    firstName: string,
+    lastName: string,
+    age: number
+} = {
+    firstName: 'Dan',
+    lastName: 'Almenar',
+    age: 42
+}
+
+// second way:
+type Person = {
+    firstName: string | number, // properties types accept unions
+    lastName: string,
+    age: number
+}
+
+// The type object accepts unions:
+type Point = number | string
+let firstPoint: Point = 1
+let secondPoint: Point = 'hello'
+console.log('Point object examples (number and string):', firstPoint, secondPoint)
+
+const danDeveloper: Person = {
+    firstName: 'Dan',
+    lastName: 'Almenar',
+    age: 42
+}
+
+console.log('person:', person)
+console.log('danDeveloper  object:', danDeveloper)
+
+// TYPE ASSERTION
+/*
+Type Assertions are a way to force a variable to be of a certain type
+when it has been initialy declared as type any.
+It can be done in two ways.
+However, the type of the asserted variable will be the same as the
+type of the variable being asserted (string => string) at compilation.
+*/
+let randomVariable: any = '3.1416'
+let piNumber1 = <number>randomVariable // type assertion
+let piNumber2 = randomVariable as number // type assertion
+
+console.log('piNumber1:', piNumber1)
+console.log('piNumber2:', piNumber2)
+console.log('types:', typeof(piNumber1), typeof(piNumber2))
+
+// FUNCTIONS
+/*
+On Functions, you can declare a function with parameters types aswell
+as the return type. If left undeclared defaults to type any.
+The void type can be use as a result type for functions that
+returns nothing.
+*/
+function addNumbers(num1: number, num2: number): number {
+    return num1 + num2
+}
+
+function addToString(num1: number, num2: number): string {
+    return `The result is: ${(num1 + num2)}`
+}
+
+function log(message: string | number): void {
+    console.log(message)
+}
+
+console.log(addNumbers(4, 3))
+console.log(addToString(4, 3))
+log("hello world")
+
+// INTERFACES
+/*
+Interfaces operate as the object type, yet they don't accept
+unions.
+readonly properties value cannot be changed
+optional properties are declared with a ?
+
+Interface can be use to declare functions, classes, and objects.
+*/
+interface PersonInterface {
+    readonly firstName: string | number, // properties types accept unions
+    lastName: string,
+    age: number
+    hobbies?: string[] // optional property
+}
+
+// can't do: interface PointInterface = number | string
+
+const newUser: PersonInterface = {
+    firstName: 'Dan',
+    lastName: 'Almenar',
+    age: 42
+}
+// optional property hobbies was left undeclared so it defaults to undefined
+
+console.log('newUser (PersonInterface) .firstName:', newUser.firstName)
+console.log('newUser.hobbies (not declared):', newUser.hobbies)
+
+// properties that are not readonly can be changed:
+newUser.lastName = 'Developer'
+newUser.hobbies = ['programming', 'playing with his kids']
+
+interface MathFunc {
+    (num1: number, num2: number): number
+}
+
+const add: MathFunc = (num1: number, num2: number): number => num1 + num2
+const substract: MathFunc = (num1: number, num2: number): number => num1 - num2
+
+// any function that accepts and return the same types as the interface
+// are part of said interface
+
